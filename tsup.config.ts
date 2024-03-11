5051513f231f03b65b7d8a573f61529937b72888
zcf0508 <zcf0508@live.com>
2024-03-11 17:01:48 +0800

    fix: postinstall running error when project first install

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,21 @@
 import type { Options } from 'tsup'
 
-export default <Options>{
-  entryPoints: [
-    'src/*.ts',
-  ],
-  clean: true,
-  format: ['cjs', 'esm'],
-  dts: true,
-  onSuccess: 'npm run build:fix',
-}
+export default [
+  {
+    entryPoints: [
+      'src/*.ts',
+    ],
+    clean: true,
+    format: ['cjs', 'esm'],
+    dts: true,
+    onSuccess: 'npm run build:fix',
+  },
+  {
+    entryPoints: [
+      'scripts/postinstall.ts',
+    ],
+    outDir: 'scripts',
+    format: ['cjs'],
+    onSuccess: 'npm run build:fix',
+  },
+] satisfies Options[]
