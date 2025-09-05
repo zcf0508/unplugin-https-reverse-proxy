1f5c48805537496e3df7c25fb7ca55d97c21d8cd
zcf0508 <zcf0508@live.com>
2025-09-05 19:03:34 +0800

    fix: prevent getting stuck during installation

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    ['~/../../../src/nuxt', {
-      enabled: true,
-      target: 'test-nuxt.abc.com',
-    }],
-  ],
+  // uncomment to enable the plugin
+  // modules: [
+  //   ['~/../../../src/nuxt', {
+  //     enabled: true,
+  //     target: 'test-nuxt.abc.com',
+  //   }],
+  // ],
 })
